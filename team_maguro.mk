912ecac9aeb628b3d7b481f90b0aff4178881149
superatmel <superatmel@gmail.com>
2012-02-12 03:34:07 +0100

    Prepare to SuperICS
    Change-Id: Ibddc3614285e2ac581cc5560b2b6abd8d478dba3

--- team_maguro.mk
+++ team_maguro.mk
@@ -1,21 +1,35 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/osr/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GN
+PRODUCT_RELEASE_NAME := GNexus
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/osr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
+
+# Tuna overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/osr/overlay/tuna
+PRODUCT_PACKAGE_OVERLAYS += vendor/osr/overlay/maguro
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := cm_maguro
+PRODUCT_NAME := team_maguro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="google/yakju/maguro:4.0.2/ICL53F/235179:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.2 ICL53F 235179 release-keys"
+
+# Inherit drm blobs
+-include vendor/osr/config/common_drm.mk
+
+# Inherit Facelock blobs
+-include vendor/osr/config/common_facelock.mk
+
+# Inherit Gapps blobs
+-include vendor/osr/config/common_gapps.mk
